045ff0a3330784725c952d2a5ce40e0e2d049059
Kristina-Isakovich <kristinaisakovich0621@gmail.com>
2021-03-01 23:13:17 +0300

    [docs] Add ES6 and ES5

---
+++ tasks/inheritance/ES5.js
@@ -0,0 +1,45 @@
+function IntBuilder(int) {
+  this.int = int
+  function random(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+}
+
+IntBuilder.prototype.plus = function (...n) {
+  return [...n].reduce((acc, it) => acc + it, this.int)
+}
+
+IntBuilder.prototype.minus = function (...n) {
+  return [...n].reduce((acc, it) => acc - it, this.int)
+}
+
+IntBuilder.prototype.multiply = function (n) {
+  let result = 1
+  for (let i = 0; i < n; i++) {
+    result *= this.int
+  }
+
+  return result
+}
+
+IntBuilder.prototype.divide = function (n) {
+  return (this.int / n).toFixed()
+}
+
+IntBuilder.prototype.mod = function (n) {
+
+}
+
+IntBuilder.prototype.get = function () {
+  return this.int
+}
+
+const intBuilder = new IntBuilder(10)
+console.log(intBuilder.plus(3, 2))
+console.log(intBuilder.minus(1, 2))
+console.log(intBuilder.multiply(2))
+console.log(intBuilder.divide(4))
+console.log(intBuilder.mod(3))
+console.log(intBuilder.get())
---
+++ tasks/inheritance/ES6.js
@@ -0,0 +1,50 @@
+class Builder {
+  constructor(str) {
+    this.str = str
+  }
+
+  get() {
+    return this.str
+  }
+}
+
+class StringBuilder extends Builder {
+  plus(...s) {
+    return this.str + s
+  }
+
+  minus(n) {
+    return this.str.slice(0, -n)
+  }
+
+  multiply(int) {
+    return this.str.repeat(int)
+  }
+
+  divide(n) {
+    return this.str.substring(Math.floor(this.str.length / n), 0)
+  }
+
+  remove(l) {
+    for (let i = 0; i < this.str.length; i++) {
+      const item = this.str[i]
+      if (item === l) {
+        this.str.slice(i)
+      }
+    }
+  }
+
+  sub(from, n) {
+    return this.str.substr(from, n)
+  }
+}
+
+const strBuilder = new StringBuilder('Hello')
+console.log(strBuilder)
+console.log(strBuilder.plus(' all!'))
+console.log(strBuilder.minus(4))
+console.log(strBuilder.multiply(3))
+console.log(strBuilder.divide(4))
+console.log(strBuilder.remove('l'))
+console.log(strBuilder.sub(1, 1))
+console.log(strBuilder.get())
