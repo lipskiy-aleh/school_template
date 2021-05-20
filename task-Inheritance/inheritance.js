a4a92b8078ecc03620761a91340e79c358c02c35
AnnaMirzaeva <75016989+AnnaMirzaeva@users.noreply.github.com>
2021-05-20 19:20:31 +0300

    linter fixes

--- task-Inheritance/inheritance.js
+++ task-Inheritance/inheritance.js
@@ -1,107 +1,120 @@
 function BaseBuilder(value) {
-    this.value = value;
+  this.value = value
 }
 
-BaseBuilder.prototype.plus = function(...args) {
-    for(let arg = 0; arg < args.length; arg++) {
-        this.value += args[arg];
-    }
+BaseBuilder.prototype.plus = function plus(...args) {
+  for (let arg = 0; arg < args.length; arg + 1) {
+    this.value += args[arg]
+  }
 
-    return this;
+  return this
 }
 
-BaseBuilder.prototype.get = function(...args) {
-    console.log(this.value);
-    return this;
+BaseBuilder.prototype.get = function get() {
+  return this.value
 }
 
 function IntBuilder(value) {
-    BaseBuilder.call(this, value);
+  BaseBuilder.call(this, value)
 
-    this.value = value;
+  this.value = value
 }
 
-IntBuilder.prototype = Object.create(BaseBuilder.prototype);
-IntBuilder.prototype.constructor = BaseBuilder;
+IntBuilder.prototype = Object.create(BaseBuilder.prototype)
+IntBuilder.prototype.constructor = BaseBuilder
 
-IntBuilder.prototype.minus = function(...args) {
-    for(let arg = 0; arg < args.length; arg++) {
-        this.value = this.value - args[arg];
-    }
+IntBuilder.prototype.minus = function minus(...args) {
+  for (let arg = 0; arg < args.length; arg + 1) {
+    this.value -= args[arg]
+  }
 
-    return this;
+  return this
 }
 
-IntBuilder.prototype.multiply = function(n) {
-    this.value = this.value * n;
+IntBuilder.prototype.multiply = function multiply(n) {
+  this.value *= n
 
-    return this;
+  return this
 }
 
-IntBuilder.prototype.divide = function(n) {
-    this.value = this.value / n;
+IntBuilder.prototype.divide = function divide(n) {
+  this.value /= n
 
-    return this;
+  return this
 }
 
-IntBuilder.prototype.mod = function(n) {
-    this.value = this.value % n;
-    
-    return this;
+IntBuilder.prototype.mod = function mod(n) {
+  this.value %= n
+
+  return this
 }
 
-IntBuilder.random = function(minValue, maxValue) {
-    return Math.floor(Math.random() * (maxValue - minValue) ) + minValue;
+IntBuilder.random = function random(minValue, maxValue) {
+  return Math.floor(Math.random() * (maxValue - minValue)) + minValue
 }
 
 class StringBuilder extends BaseBuilder {
-    constructor(value) {
-        super(value);
+  constructor(value) {
+    super(value)
 
-        this.value = value;
+    this.value = value
+  }
+
+  minus(cutNumber) {
+    this.value = this.value.slice(0, this.value.length - cutNumber)
+
+    return this
+  }
+
+  multiply(repeatNumber) {
+    this.value = this.value.repeat(repeatNumber)
+
+    return this
+  }
+
+  divide(divideNumber) {
+    const endValue = Math.floor(this.value.length / divideNumber)
+
+    this.value = this.value.slice(0, endValue)
+
+    return this
+  }
+
+  remove(removeString) {
+    const a = this.value.search(removeString)
+
+    if (a !== -1) {
+      this.value = this.value.slice(0, a) + this.value.slice(a + removeString.length)
+      this.remove(removeString)
     }
 
-    minus(cutNumber) {
-        this.value = this.value.slice(0, this.value.length - cutNumber);
+    return this
+  }
 
-        return this;
-    }
+  sub(from, n) {
+    this.value = this.value.slice(from, from + n)
 
-    multiply(repeatNumber) {
-        this.value = this.value.repeat(repeatNumber);
-
-        return this;
-    }
-
-    divide(divideNumber) {
-        const endValue = Math.floor(this.value.length / divideNumber)
-
-        this.value = this.value.slice(0, endValue);
-
-        return this;
-    }
-
-    remove = (removeString) => {
-        let a = this.value.search(removeString);
-
-        if(a !== -1) {
-            this.value = this.value.slice(0, a) + this.value.slice(a + removeString.length);
-            this.remove(removeString);
-        }
-
-        return this;
-    }
-
-    sub(from, n) {
-        this.value = this.value.slice(from, from + n);
-
-        return this;
-    }
+    return this
+  }
 }
 
+IntBuilder.random(10, 100)
 
-console.log(IntBuilder.random(0, 100));
+const intBuilder = new IntBuilder(10)
+intBuilder
+  .plus(2, 3, 2)
+  .minus(1, 2)
+  .multiply(2)
+  .divide(4)
+  .mod(3)
+  .get()
 
-let intBuilder = new IntBuilder(10).plus(2, 3, 2).minus(1, 2).multiply(2).divide(4).mod(3).get();
-
-let strBuilder = new StringBuilder("Hello").plus(" all", "!").minus(4).multiply(3).divide(4).remove('l').sub(1, 1).get();
+const strBuilder = new StringBuilder('Hello')
+strBuilder
+  .plus(' all', '!')
+  .minus(4)
+  .multiply(3)
+  .divide(4)
+  .remove('l')
+  .sub(1, 1)
+  .get()
